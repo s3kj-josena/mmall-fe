8437d4f4cfc07160651a514d04d853e3c951f01f
Josena <s3kj.josena@gmail.com>
2019-03-18 17:54:41 +0800

    pass-update

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ var configs = {
         'login' : ['./src/page/login/index.js'],
         'register' : ['./src/page/register/index.js'],
         'pass-reset' : ['./src/page/pass-reset/index.js'],
+        'pass-update' : ['./src/page/pass-update/index.js'],
         'user-center' : ['./src/page/user-center/index.js'],
         'list' : ['./src/page/list/index.js'],
         'detail' : ['./src/page/detail/index.js']
@@ -62,6 +63,7 @@ var configs = {
         new HtmlWebpackPlugin(getHtmlConfig('login')),
         new HtmlWebpackPlugin(getHtmlConfig('register')),
         new HtmlWebpackPlugin(getHtmlConfig('pass-reset')),
+        new HtmlWebpackPlugin(getHtmlConfig('pass-update')),
         new HtmlWebpackPlugin(getHtmlConfig('user-center')),
         new HtmlWebpackPlugin(getHtmlConfig('list')),
         new HtmlWebpackPlugin(getHtmlConfig('detail')),
